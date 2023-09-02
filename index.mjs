55e2334dd03d37e51fdc22b039eabfe77fff0701
miki1513 <25953044-miki1513@users.noreply.replit.com>
2023-09-02 21:01:47 +0000

    Initial commit

---
+++ index.mjs
@@ -0,0 +1,14 @@
+import Client from "msgroom";
+import { generate, count } from "random-words";
+
+const client = new Client("ReplBot [r!]", [ "r!" ]);
+await client.connect();
+client.commands.ping = () => "pong";
+
+client.commands.repeat = (reply, ...args) => {
+    return args.join(" ");
+};
+client.commands.vote = () => "Voted!";
+client.commands.vencord = () => "Vencord is a Discord client mod!";
+client.commands.betterdiscord = () => "BetterDiscord is a Discord client mod!";
+client.commands.sendRandomWordToConsole = () => console.log(generate({ minLength: 1, maxLength: 1000000 }));;
