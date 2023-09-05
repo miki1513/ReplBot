79560d4d45aebdfc3dce1102679f657f264630ee
miki1513 <142451601+miki1513@users.noreply.github.com>
2023-09-05 09:48:17 +0000

    added a one thing in bot info

--- index.mjs
+++ index.mjs
@@ -3,6 +3,7 @@ import { generate, count } from "random-words";
 
 const client = new Client("ReplBot [repl!]", [ "repl!" ]);
 await client.connect();
+console.log("Connected to MsgRoom servers")
 client.commands.ping = () => "pong";
 
 client.commands.repeat = (reply, ...args) => {
@@ -11,4 +12,5 @@ client.commands.repeat = (reply, ...args) => {
 client.commands.vote = () => "Voted!";
 client.commands.vencord = () => "Vencord is a Discord client mod!";
 client.commands.betterdiscord = () => "BetterDiscord is a Discord client mod!";
-client.commands.sendRandomWordToConsole = () => console.log(generate({ minLength: 1, maxLength: 1000000 }));;
+client.commands.randomword = () => (generate({ minLength: 1, maxLength: 1000000 }));;
+client.commands.botinfo = () => "Server: Replit. Bot username: ReplBot. Source? Not yet"
